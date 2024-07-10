c1ab74c9617b7e87b73f0d8fc4c464d0bf2b9cef
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-07-10 13:14:09 -0300

    Operadores Relacionais no C#

---
+++ CursoCSharp/CursoCSharp/Fundamentos/OperadoresRelacionais.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class OperadoresRelacionais
+    {
+        public static void Executar()
+        {
+            //double nota = 6.0;
+            Console.WriteLine("Digite a nota: ");
+            double.TryParse(Console.ReadLine(), out double nota);
+            double notaDecorte = 7.0;
+
+            Console.WriteLine("Nota inválida? {0}", nota > 10.0);
+            Console.WriteLine("Nota inválida? {0}", nota < 0.0);
+            Console.WriteLine("Perfeito? {0}", nota == 10.0);
+            Console.WriteLine("Tem como melhorar? {0}", nota != 10.0);
+            Console.WriteLine("Passou por média? {0}", nota >= notaDecorte);
+            Console.WriteLine("Recuperação? {0}", nota < notaDecorte);
+            Console.WriteLine("Reprovado? {0}", nota <= 3.0);
+
+        }
+    }
+}
