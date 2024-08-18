4151f238b54f27afaa905bd805fe5397ac59ac16
eltonsa75 <eltonsa75@hotmail.com>
2024-08-18 17:26:48 -0300

    Criando APICatalogo

---
+++ CursoCSharp/CursoCSharp/Colecoes/ColecoesDictionary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Colecoes
+{
+    internal class ColecoesDictionary
+    {
+        public static void Executar()
+        {
+            // Chave valor Dictionary
+            var filmes = new Dictionary<int, string>();
+
+            filmes.Add(2000, "Gladiador");
+            filmes.Add(2002, "Homem-Aranha");
+            filmes.Add(2004, "Os Incriveis");
+            filmes.Add(2006, "O Grande Truque");
+
+
+            if (filmes.ContainsKey(2004))
+            {
+                Console.WriteLine("2004: " + filmes[2004]);
+                Console.WriteLine("2004: " + filmes.GetValueOrDefault(2004));
+            }
+
+            Console.WriteLine(filmes.ContainsValue("Amnésia"));
+
+            Console.WriteLine($"Removeu? {filmes.Remove(2004)}");
+
+            filmes.TryGetValue(2006, out string filmes2006);
+            Console.WriteLine($"Filme {filmes2006}!");
+
+            // Percorrendo do Dictionary
+
+            foreach (var chave in filmes.Keys)
+            {
+                Console.WriteLine(chave);
+            }
+
+            foreach (var valor in filmes.Values)
+            {
+                Console.WriteLine(valor); 
+            }
+
+            foreach (KeyValuePair<int, string> filme in filmes)
+            {
+                Console.WriteLine($"{filme.Value} é de {filme.Key}.");
+            }
+
+            foreach (var filme in filmes)
+            {
+                Console.WriteLine($"{filme.Value} é de {filme.Key}.");
+            }
+        }
+    }
+}
--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -66,7 +66,8 @@ namespace CursoCSharp {
                 {"Set - Coleções", ColecoesSet.Executar},
                 {"Queue - Coleções", ColecoesQueue.Executar},
                 {"Igualdade - Coleções", Igualdade.Executar},
-                {"Stack - Coleções", ColecoesStack.Executar},
+                {"Stack - Coleções", ColecoesStack.Executar},                
+                {"Dictionary - Coleções", ColecoesDictionary.Executar},
             });
 
             central.SelecionarEExecutar();
