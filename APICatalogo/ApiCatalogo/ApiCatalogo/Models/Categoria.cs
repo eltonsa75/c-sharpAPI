c246aedacd7a03f56aad56027340953120b3ad20
eltonsa75 <eltonsa75@hotmail.com>
2024-08-19 08:20:49 -0300

    Trabalhando com DataAnnotations. DataAnnotations.Schema

--- APICatalogo/ApiCatalogo/ApiCatalogo/Models/Categoria.cs
+++ APICatalogo/ApiCatalogo/ApiCatalogo/Models/Categoria.cs
@@ -1,11 +1,24 @@
-﻿namespace ApiCatalogo.Models;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace ApiCatalogo.Models;
+[Table("Categorias")]
 public class Categoria
 {
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+    [Key]
     public int CategoriaId { get; set; }
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
 
     // Relacionamento um para muitos
-    public ICollection<Produto> Produtos { get; set; }
+    public ICollection<Produto>? Produtos { get; set; }
 }
