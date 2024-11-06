e07aa37b720e55090af58e47c87d811a9d6006b2
eltonsa75 <elton.andrade@refuturiza.com.br>
2024-11-06 09:13:23 -0300

    Trabalhando com classe selada

---
+++ CursoCSharp/CursoCSharp/OO/Sealed.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.OO
+{
+
+    sealed class SemFilho
+    {
+        public double ValorDaFortuna()
+        {
+            return 1_407_033.65;
+        }
+    }
+
+    internal class Sealed
+    {
+        public static void Executar()
+        {
+
+        }
+    }
+}
