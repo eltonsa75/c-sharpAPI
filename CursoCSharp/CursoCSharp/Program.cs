e4eb521604725298417dec052e8441f6aff452eb
eltonsa75 <elton.andrade@refuturiza.com.br>
2024-11-10 10:36:24 -0300

    Lendo Arquivo da API

--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -98,6 +98,7 @@ namespace CursoCSharp {
 
                 // API
                 {"Primeiro Arquivo - Usando API ", PrimeiroArquivo.Executar},
+                {"Lendo Arquivo - Usando API ", LendoArquivos.Executar},
                
                
             });
