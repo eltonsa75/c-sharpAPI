a93d88e637c157df65f46fdd578d091ec92ae3b3
eltonsa75 <elton.andrade@refuturiza.com.br>
2024-11-10 14:44:19 -0300

    Trabalhando com Nullables

---
+++ CursoCSharp/CursoCSharp/TopicosAvancados/Nullables.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.TopicosAvancados
+{
+    internal class Nullables
+    {
+
+        static int num3;
+        public static void Executar()
+        {
+            Nullable<int> num1 = null;
+            int? num2 = null;
+
+            if(num1.HasValue)
+            {
+                Console.WriteLine("Valor de num: {0}", num1);
+            } else
+            {
+                Console.WriteLine("a variável não possui valor");
+            }
+
+            int valor = num1 ?? 1000;
+            Console.WriteLine(valor);
+
+            bool? booleana = null;
+            bool resultado = booleana.GetValueOrDefault();
+            Console.WriteLine(resultado);
+
+        }   
+    }
+}
