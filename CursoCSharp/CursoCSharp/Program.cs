3859b605ef06b120797dfcf3d23406af425e8186
eltonsa75 <elton.andrade@refuturiza.com.br>
2024-11-10 12:44:38 -0300

    Trabalhando com Formatação de DateTime

--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -103,6 +103,7 @@ namespace CursoCSharp {
                 {"Diretorios - Usando API ", Diretorios.Executar},
                 {"Exemplo DirectoryInfo - Usando API ", ExemploDirectoryInfo.Executar},
                 {"Exemplo Path - Usando API ", ExemploPath.Executar},
+                {"Exemplo Datetime - Usando API ", ExemploDateTime.Executar},
                
                
             });
