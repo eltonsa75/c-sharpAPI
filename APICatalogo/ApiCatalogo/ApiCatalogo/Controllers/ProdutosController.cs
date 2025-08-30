80358673edb7918aeb63d2b3ec46f57dd65bcbb3
Elton Andrade <eltonsa75@hotmail.com>
2025-08-30 20:21:36 -0300

    Criando o método Delete

--- APICatalogo/ApiCatalogo/ApiCatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/ApiCatalogo/ApiCatalogo/Controllers/ProdutosController.cs
@@ -69,5 +69,18 @@ namespace ApiCatalogo.Controllers
             return Ok(produto);
         }
 
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if(produto is null)
+            {
+                return NotFound("Produto não encontrado...");
+            }
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return Ok(produto);
+        }   
+
     }
 }
